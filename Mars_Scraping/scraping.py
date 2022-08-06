7d38d7f808d6e341d30c2eb7b5e30387d3167992
Mel Phillips <melphillips@Mels-MBP-2.lan>
2022-08-05 21:39:05 -0400

    adding challenge code to repo.

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         'news_paragraph': news_paragraph,
         'featured_image': featured_image(browser),
         'facts': mars_facts(),
-        'last_modified': dt.datetime.now()
+        'last_modified': dt.datetime.now(),
+        'hemispheres': hemisphere_img_urls(browser)
     }
 
     #Stop webdriver and return data
@@ -100,6 +101,43 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     return df.to_html()
+
+def hemisphere_img_urls(browser):
+
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # Parse the resulting html with soup
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+    results = hemi_soup.find_all('div', class_='description')
+    
+    try:
+        for result in results:
+            
+            # Use first word of titles to find links
+            title = result.find('h3').text
+            browser.links.find_by_partial_text(title).click()
+            
+            # Set up HTML parser again
+            html = browser.html
+            hemi_soup = soup(html, 'html.parser')
+            
+            href = hemi_soup.find('a', text='Sample')['href']
+            hemisphere_image_urls.append(
+                {'img_url': url + href, 
+                'title': title})
+            browser.back()
+    except AttributeError:
+        return None
+
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
     # If running as script, print scraped data
